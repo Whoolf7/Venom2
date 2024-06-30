ce5c90674add8bf4df764b3c3a012ea7b6ab93b3
Whoolf7 <debashishroy740@gmail.com>
2024-06-30 16:03:03 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -16,12 +16,11 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = 'TOKEN'
+TOKEN = '7411841661:AAHjoP1iUoqEDyQtCflimeo9T3WaLrk3aBY'
 MONGO_URI = 'mongodb+srv://VENOMxCRAZY:CRAZYxVENOM@cluster0.ythilmw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsAllowInvalidCertificates=true'
-FORWARD_CHANNEL_ID = -1002224570220
-CHANNEL_ID = -1002224570220
-error_channel_id = -1002224570220
-
+FORWARD_CHANNEL_ID = -1002244614881
+CHANNEL_ID = -1002244614881
+error_channel_id = -1002244614881
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
